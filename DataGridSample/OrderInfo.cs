8e7445dc9b789a8e165f5c80a83e38e1e558eaf7
Sweatha Bharathi <sweatha.bharathi@syncfusion.com>
2025-05-02 15:15:37 +0530

    To select a Row by Cell Value in WinForms SfDataGrid.

---
+++ DataGridSample/OrderInfo.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace DataGridSample
+{
+  
+    public class OrderInfo : INotifyPropertyChanged
+    {
+        private string orderID;
+        private string customerId;
+        private string country;
+        private string customerName;
+        private string shippingCity;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string OrderID
+        {
+            get => orderID;
+            set
+            {
+                if (orderID != value)
+                {
+                    orderID = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string CustomerID
+        {
+            get => customerId;
+            set
+            {
+                if (customerId != value)
+                {
+                    customerId = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string CustomerName
+        {
+            get => customerName;
+            set
+            {
+                if (customerName != value)
+                {
+                    customerName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Country
+        {
+            get => country;
+            set
+            {
+                if (country != value)
+                {
+                    country = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string ShipCity
+        {
+            get => shippingCity;
+            set
+            {
+                if (shippingCity != value)
+                {
+                    shippingCity = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public OrderInfo(string orderId, string customerName, string country, string customerId, string shipCity)
+        {
+            this.OrderID = orderId;
+            this.CustomerName = customerName;
+            this.Country = country;
+            this.CustomerID = customerId;
+            this.ShipCity = shipCity;
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # How-to-select-a-Row-by-Cell-Value-in-WinForms-SfDataGrid
-This demo shows how to select a Row by Cell Value in WinForms SfDataGrid
+
+In a WinForms [DataGrid](https://www.syncfusion.com/winforms-ui-controls/datagrid), a row can be selected based on a specific cell value. A **TextBox** has been implemented to allow users to enter the desired cell value. If the entered value matches any value within the DataGrid columns, the corresponding row is retrieved and set as the [SelectedItem](https://help.syncfusion.com/cr/windowsforms/Syncfusion.WinForms.DataGrid.SfDataGrid.html#Syncfusion_WinForms_DataGrid_SfDataGrid_SelectedItem).
+ 
+ ```csharp
+  private void SelectRow(object sender, EventArgs e)
+   {
+      var rowData = orderInfoCollection.Orders.FirstOrDefault(data => data.OrderID.ToString().ToLower() == textBox1.Text.ToLower() ||
+                                             data.CustomerID.ToString().ToLower() == textBox1.Text.ToLower() ||
+                                             data.CustomerName.ToString().ToLower() == textBox1.Text.ToLower() ||
+                                             data.ShipCity.ToString().ToLower() == textBox1.Text.ToLower() ||
+                                             data.Country.ToString().ToLower() == textBox1.Text.ToLower());
+
+      this.sfDataGrid1.SelectedItem = rowData;
+   }
+
+ ```
+
+
+
+![SelectRowByCellValue.gif](https://support.syncfusion.com/kb/agent/attachment/article/18660/inline?token=eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjM5OTU2Iiwib3JnaWQiOiIzIiwiaXNzIjoic3VwcG9ydC5zeW5jZnVzaW9uLmNvbSJ9.f86Wx927Rdbog61whT624ZNQ-m9vmnjEfEeIUQIDmNA)
+
+
+Take a moment to peruse the [Winforms - DataGrid Selection UG Documentation ](https://help.syncfusion.com/windowsforms/datagrid/selection), to learn more about DataGrid Selections with examples.
